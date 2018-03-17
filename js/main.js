2f41d4aa4f85e12e65c5c73194a5534016b16c98
Cloud <9652227+xPixv@users.noreply.github.com>
2018-03-18 03:08:02 +0800

    Beta 0.1.0

---
+++ js/main.js
@@ -0,0 +1,46 @@
+const txtBotName = document.getElementById("bot-name");
+const txtInput = document.getElementById("ske-input");
+const btnExtract = document.getElementById("ske-extract");
+const btnCopy = document.getElementById("ske-copy");
+const btnExport = document.getElementById("ske-export");
+const btnClear = document.getElementById("ske-clear");
+const txtOutput = document.getElementById("ske-output");
+const keyRegex = /(?:(?:([A-Z0-9])(?!\1{4})){5}-){2,5}[A-Z0-9]{5}/g;
+const unique = a => [...new Set(a)];
+
+btnExtract.onclick = () => {
+    txtOutput.value = "";
+
+    const keys = unique(txtInput.value.match(keyRegex));
+    if (keys.length > 0) {
+        txtOutput.value = "!redeem " + txtBotName.value + " " + keys.join(",");
+        txtOutput.focus();
+    } else {
+        M.toast({
+            html: '无 Steam Key！'
+        });
+    }
+    M.updateTextFields();
+    M.textareaAutoResize(txtOutput);
+    M.textareaAutoResize(txtInput);
+}
+
+btnCopy.onclick = () => {
+    txtOutput.select();
+    document.execCommand("Copy");
+    M.toast({
+        html: '已复制！'
+    });
+}
+
+btnClear.onclick = () => {
+    txtInput.value = "";
+    txtOutput.value = "";
+    M.updateTextFields();
+    M.textareaAutoResize(txtOutput);
+    M.textareaAutoResize(txtInput);
+
+    M.toast({
+        html: '已清空！'
+    });
+}
