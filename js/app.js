6c7be5bbea345fe5972cf6c32ff7d2daf6e5c158
Cloud <9652227+xPixv@users.noreply.github.com>
2018-03-19 20:15:18 +0800

    Update register

--- js/app.js
+++ js/app.js
@@ -58,14 +58,12 @@
 
     // Register Services Worker
     if ("serviceWorker" in navigator) {
-        window.addEventListener("load", function () {
-            navigator.serviceWorker.register("service-worker.js").then(function (registration) {
-                // Registration was successful
-                console.log("ServiceWorker registration successful with scope: ", registration.scope);
-            }, function (err) {
-                // registration failed :(
-                console.log("ServiceWorker registration failed: ", err);
-            });
+        navigator.serviceWorker.register("service-worker.js").then(function (registration) {
+            // Registration was successful
+            console.log("ServiceWorker registration successful with scope: ", registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log("ServiceWorker registration failed: ", err);
         });
     }
 })();
