4c437761ff3faba04659b85271b438612d4b9283
Cloud <cloudswift@qq.com>
2018-03-19 20:05:18 +0800

    PWA Update, add icons, move service-workers,update manifest and misc.

--- js/app.js
+++ js/app.js
@@ -1,55 +1,71 @@
+(function () {
+    "use strict";
 
-const txtBotName = document.getElementById("bot-name");
-const txtInput = document.getElementById("ske-input");
-const btnExtract = document.getElementById("ske-extract");
-const btnCopy = document.getElementById("ske-copy");
-const btnExport = document.getElementById("ske-export");
-const btnClear = document.getElementById("ske-clear");
-const txtOutput = document.getElementById("ske-output");
+    const txtBotName = document.getElementById("bot-name");
+    const txtInput = document.getElementById("ske-input");
+    const btnExtract = document.getElementById("ske-extract");
+    const btnCopy = document.getElementById("ske-copy");
+    const btnExport = document.getElementById("ske-export");
+    const btnClear = document.getElementById("ske-clear");
+    const txtOutput = document.getElementById("ske-output");
 
-const keyRegex = /(?:(?:([A-Z0-9])(?!\1{4})){5}-){2,5}[A-Z0-9]{5}/g;
-const unique = a => [...new Set(a)];
+    const keyRegex = /(?:(?:([A-Z0-9])(?!\1{4})){5}-){2,5}[A-Z0-9]{5}/g;
+    const unique = a => [...new Set(a)];
 
-btnExtract.onclick = () => {
-    txtOutput.value = "";
+    btnExtract.onclick = () => {
+        txtOutput.value = "";
 
-    const keys = unique(txtInput.value.match(keyRegex));
-    if (keys.length > 0) {
+        const keys = unique(txtInput.value.match(keyRegex));
+        if (keys.length > 0) {
+            M.toast({
+                html: "Found " + keys.length + " Keys!"
+            });
+            txtOutput.value = "!redeem " + (txtBotName.value == "" ? "" : (txtBotName.value + " ")) + keys.join(",");
+            txtOutput.focus();
+        } else {
+            M.toast({
+                html: "Keys not Found!"
+            });
+        }
+        M.updateTextFields();
+        M.textareaAutoResize(txtOutput);
+        M.textareaAutoResize(txtInput);
+    };
+
+    btnCopy.onclick = () => {
+        txtOutput.select();
+        document.execCommand("Copy");
         M.toast({
-            html: "Found " + keys.length + " Keys!"
+            html: "Copied!"
         });
-        txtOutput.value = "!redeem " + (txtBotName.value == "" ? "" : (txtBotName.value + " ")) + keys.join(",");
-        txtOutput.focus();
-    } else {
+    };
+
+    btnExport.onclick = () => {
+
+    };
+
+    btnClear.onclick = () => {
+        txtInput.value = "";
+        txtOutput.value = "";
+        M.updateTextFields();
+        M.textareaAutoResize(txtOutput);
+        M.textareaAutoResize(txtInput);
+
         M.toast({
-            html: "Keys not Found!"
+            html: "Cleared!"
+        });
+    };
+
+    // Register Services Worker
+    if ("serviceWorker" in navigator) {
+        window.addEventListener("load", function () {
+            navigator.serviceWorker.register("service-worker.js").then(function (registration) {
+                // Registration was successful
+                console.log("ServiceWorker registration successful with scope: ", registration.scope);
+            }, function (err) {
+                // registration failed :(
+                console.log("ServiceWorker registration failed: ", err);
+            });
         });
     }
-    M.updateTextFields();
-    M.textareaAutoResize(txtOutput);
-    M.textareaAutoResize(txtInput);
-};
-
-btnCopy.onclick = () => {
-    txtOutput.select();
-    document.execCommand("Copy");
-    M.toast({
-        html: "Copied!"
-    });
-};
-
-btnExport.onclick = () => {
-
-};
-
-btnClear.onclick = () => {
-    txtInput.value = "";
-    txtOutput.value = "";
-    M.updateTextFields();
-    M.textareaAutoResize(txtOutput);
-    M.textareaAutoResize(txtInput);
-
-    M.toast({
-        html: "Cleared!"
-    });
-};
+})();
---
+++ service-worker.js
@@ -0,0 +1,45 @@
+var cacheName = "ske-dev-v0.2";
+
+var filesToCache = [
+    "/",
+    "/index.html",
+    "/favicon.ico",
+    "/manifest.json",
+    "/img/Background.png",
+    "/img/SteamLogo.png",
+    "/css/app.css",
+    "/css/materialize.min.css",
+    "/css/material-icons.css",
+    "/fonts/material-icons/MaterialIcons-Regular.woff2",
+    "/js/app.js",
+    "/js/materialize.min.js"
+];
+
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener("activate", function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
