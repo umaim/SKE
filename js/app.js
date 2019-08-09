9c418effb1f3bb2fbaaf74cf4d4ab4fa34957873
Cloud <9652227+xPixv@users.noreply.github.com>
2019-08-10 02:17:41 +0800

    fix: fix service worker

--- js/app.js
+++ js/app.js
@@ -346,10 +346,10 @@
     if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('service-worker.js').then(function ( /*registration*/) {
             // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
+            // console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (/*err*/) {
             // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
+            // console.log('ServiceWorker registration failed: ', err);
         });
     }
 
